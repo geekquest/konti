24ecc4529d2df4f7fdbcc8a511a0589b4502b542
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2023-02-27 00:02:36 +0200

    inject GreypotClient in constructor

--- app/Http/Controllers/AllContributorsReportController.php
+++ app/Http/Controllers/AllContributorsReportController.php
@@ -8,11 +8,16 @@ use App\Http\Client\GreypotClient;
 
 class AllContributorsReportController extends BaseController
 {
+
+    private $greypot;
+    
+    public function __construct(GreypotClient $greypot)
+    {
+        $this->greypot = $greypot;
+    }
+
     public function download()
     {
-        $greypot = new GreypotClient();
-
-
         $data = [
             'contributors' => [
                 [
@@ -27,7 +32,7 @@ class AllContributorsReportController extends BaseController
 
         $template = file_get_contents(resource_path('report-template/contributors.html'));
         
-        $reportData = $greypot->generatePDF($template, $data);
+        $reportData = $this->greypot->generatePDF($template, $data);
 
         return response()->streamDownload(function() use ($reportData) {
                 echo $reportData;
