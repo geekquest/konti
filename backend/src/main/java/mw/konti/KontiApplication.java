512f51e74d255820cd610950e07a579db460c37a
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2023-03-12 15:24:18 +0200

    initialize springboot and react ui

---
+++ backend/src/main/java/mw/konti/KontiApplication.java
@@ -0,0 +1,13 @@
+package mw.konti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mw/konti/KontiApplicationTests.java
@@ -0,0 +1,13 @@
+package mw.konti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
