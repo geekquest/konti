db815c5e72aa0db7ce453b78c1f8451a64f538c2
Wongani Kaluwa <kaluawongan@gmail.com>
2023-03-12 16:52:50 +0200

    Created a new Branch
    Created a brand first-task and then added some navigation to the initial ui

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,17 +1,37 @@
 import * as React from 'react'
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react'
 
 export default function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <div>
-        <nav>Navbar</nav>
-        <div>Sidebar</div>
-        <section>Main</section>
-      </div>
+      <Grid
+        templateAreas={`"header header"
+                        "nav main"
+                        "nav footer"`}
+        gridTemplateRows={'50px 1fr 30px'}
+        gridTemplateColumns={'150px 1fr'}
+        h='auto'
+        w='100%'
+        gap='1'
+        color='blackAlpha.700'
+        fontWeight='bold'
+      >
+        <GridItem pl='2' bg='orange.300' area={'header'}>
+          Header
+        </GridItem>
+        <GridItem pl='2' bg='pink.300' area={'nav'}>
+          Nav
+        </GridItem>
+        <GridItem pl='2' bg='green.300' area={'main'}>
+          Main
+        </GridItem>
+        <GridItem pl='2' bg='blue.300' area={'footer'}>
+          Footer
+        </GridItem>
+      </Grid>
     </ChakraProvider>
   )
 }
